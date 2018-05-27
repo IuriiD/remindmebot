259b5a3777fe73538d9b9deea0b659807e21f9bb
Iurii Dziuban <iurii.dziuban@gmail.com>
2018-05-27 12:20:52 +0000

    toHeroku

--- helpers/processMessage.js
+++ helpers/processMessage.js
@@ -6,7 +6,8 @@
     2) no reminders editing (delete and create a new reminder instead if needed);
 */
 
-const API_AI_TOKEN = 'c2a8b983845543e2b0d54018ad01d2d1';
+const API_AI_TOKEN = process.env.API_AI_TOKEN;
+//'c2a8b983845543e2b0d54018ad01d2d1';
 const apiAiClient = require('apiai')(API_AI_TOKEN);
 
 const functions = require('./functions');
@@ -19,7 +20,6 @@ module.exports = (event) => {
     let senderId = "";
     let message = "";
     let buttonTemplate = "";
-    let quickButtons = "";
     let speech = "";
 
     if ("message" in event) {
