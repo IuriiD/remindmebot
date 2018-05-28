1d141e2e6a7f128af10c7e57f9b824ca62c53b5e
Iurii Dziuban <iurii.dziuban@gmail.com>
2018-05-28 19:24:46 +0000

    process.env.PORT || 5000

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.listen(5000, function() {console.log('Webhook server is listening, port 5000')});
+app.listen(process.env.PORT || 5000, function() {console.log('Webhook server is listening, port 5000')});
 
 const verificationController = require('./controllers/verification');
 const messageWebhookController = require('./controllers/messageWebhook');
