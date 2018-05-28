61aa0275d301826375926d0d5d93bdfe019a5f09
Iurii Dziuban <iurii.dziuban@gmail.com>
2018-05-28 19:04:56 +0000

    Heroku

--- controllers/verification.js
+++ controllers/verification.js
@@ -1,4 +1,4 @@
-module.exports = (req, res) => {
+/*module.exports = (req, res) => {
  const hubChallenge = req.query['hub.challenge'];
  const hubMode = req.query['hub.mode'];
  const verifyTokenMatches = (req.query['hub.verify_token'] === 'remindmebot');
@@ -9,7 +9,7 @@ module.exports = (req, res) => {
    console.log('Verification failedLOLOLOLOLO LOLOL');
    res.status(403).end();
   }
-};
+};*/
 
 module.exports = (req, res) => {
     const hubChallenge = req.query['hub.challenge'];
